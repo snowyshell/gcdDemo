c20165d50b9e8f1fbf9365b92c6725e7f25319a4
Jianmei <Jianmei@shijianmeideiMac.local>
2017-08-23 18:53:23 +0800

    Initial Commit

---
+++ gcd 线程间通信/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gcd 线程间通信
+//
+//  Created by Jianmei on 2017/8/23.
+//  Copyright © 2017年 Jianmei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gcd 线程间通信/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gcd 线程间通信
+//
+//  Created by Jianmei on 2017/8/23.
+//  Copyright © 2017年 Jianmei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
